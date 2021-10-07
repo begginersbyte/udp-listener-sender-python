d4859d94d8a06510503d76b2a4a725647531d4d6
kevn-gonz <kevin17t@gmail.com>
2021-10-07 15:45:31 -0600

    small changes

--- udplistener.py
+++ udplistener.py
@@ -3,7 +3,7 @@ import os
 
 UDP_IP = "0.0.0.0" #listen to any trafic on the host
 UDP_PORT = int(os.environ['UDP_PORT']) #Create an environment variable called UDP_PORT and specify the port number(-e UDP_PORT=4444)
-BUFFER_SIZE  = int(os.environ['BUFFER_SIZE'])#Create an environment variable called BUFFER_SIZE and specify it (-e BUFFER_SIZE=1024)
+BUFFER_SIZE  = int(os.environ['BUFFER_SIZE']) #Create an environment variable called BUFFER_SIZE and specify it (-e BUFFER_SIZE=1024)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
@@ -13,5 +13,5 @@ while True:
     message = bytesAddressPair[0]
     clientMsg = message.decode("utf-8") #format data, decode utf-8
     f=open(f"./logs/{UDP_PORT}.log", "a") #open/create file to store messages. The name of the file is the port of the Container
-    f.write(clientMsg) #write messages to file
+    f.write(clientMsg) #write messages to .log files to be consumed later on by filebeat
     f.flush()
